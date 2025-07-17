71d7a50ec2e1d198159acd231056e60c4b8c7592
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2025-07-17 20:27:11 +0330

    Add solution for challenge

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Dropdown from "./components/Dropdown/Dropdown";
+import Speech from "./pages/Speech";
+import MyArchive from "./pages/MyArchive";
+import Icon from "./components/Icon/Icon";
+
+const App = () => {
+  return (
+    <div className="grid grid-cols-[166px_1fr] w-full">
+      <Sidebar />
+      <main className="w-11/12 mx-auto mb-[330px]">
+        <header className="flex items-center justify-end">
+          <div className=" mt-[55px] ">
+            <Dropdown
+              title="مهمان"
+              items={[
+                {
+                  id: crypto.randomUUID(),
+                  text: "خروج",
+                  icon: <Icon width={14} height={15} name="logout" color="none" className="text-[#00BA9F]" />,
+                },
+              ]}
+              size={"w-[121px]"}
+            >
+              <Icon width={20} height={19} name="user" color="none" className="text-[#00BA9F]" />
+            </Dropdown>
+          </div>
+        </header>
+        {/* main Content */}
+        <Speech />
+        {/* <MyArchive/> */}
+      </main>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Icon/Icon.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+
+// components/Icon.jsx
+export default function Icon({
+  name, // The ID from the sprite
+  size = 24,
+  width,
+  height, // Default size
+  color = "currentColor", // Default color
+  className = "",
+  ...props
+}) {
+  
+  return (
+    <svg
+      width={width || size}
+      height={height || size}
+      fill={color}
+      className={`icon icon-${name} ${className}`}
+      {...props}
+      viewBox={`0 0 ${width} ${height}`}
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <use xlinkHref={`/sprite.svg#${name}`} />
+    </svg>
+  );
+}
