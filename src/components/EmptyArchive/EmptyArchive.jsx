ad1072657e0071859e29fa7a7f6f4977f8a3b281
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2025-07-28 15:54:24 +0330

    modified

--- src/components/EmptyArchive/EmptyArchive.jsx
+++ src/components/EmptyArchive/EmptyArchive.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const EmptyArchive = ({ title, subtitle }) => {
   return (
     <div className={"emptyContent"}>
-      <img src={"../../../src/assets/images/empty-ad.png"} alt="icon " />
+      <img src={"./empty-ad.png"} alt="icon " />
       <div className={"emptyInfo"}>
         <h5 className={"emptyTitle"}>{title}</h5>
         <p className={"emptyDesc"}>{subtitle}</p>
